38a7bd2a890a9aa519bb28416cd3bc9961bfe860
Brian Flynn <bdflynny@gmail.com>
2016-01-14 20:41:10 -0500

    changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
- root "posts#welcome"
+ root "welcome#index"
 
  resources :users do
   resources :posts
