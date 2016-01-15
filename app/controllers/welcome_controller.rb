3e4f5f4b0decbdf3dda07acdff2b20700b735c70
Brian Flynn <bdflynny@gmail.com>
2016-01-14 19:00:31 -0500

    initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @user_count = User.all.count
+    @users = User.all
+  end
+
+
+end
---
+++ config/initializers/aws.rb
@@ -0,0 +1,4 @@
+AWS.config(
+  access_key_id: "AKIAIRJ4GHRRHZHYNKCQ",
+  secret_access_key: "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"
+  )
