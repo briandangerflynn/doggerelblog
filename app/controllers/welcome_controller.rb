5114a1e9292fde1446db2023781772f6db81578f
Brian Flynn <bdflynny@gmail.com>
2016-01-14 20:37:04 -0500

    changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    @posts = Post.all
     @user_count = User.all.count
     @users = User.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
- root "posts#index"
+ root "posts#welcome"
 
  resources :users do
   resources :posts
