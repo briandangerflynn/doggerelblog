a514feeaf6dbd4588972805633fb051c24062ef0
Brian Flynn <bdflynny@gmail.com>
2016-01-25 18:11:45 -0500

    update

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
 AWS.config(
-  access_key_id: "AKIAJ2Z4KDCI5LQNQCNQ",
-  secret_access_key: "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"
+  access_key_id: "AKIAIRJ4GHRRHZHYNKCQ",
+  secret_access_key: "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"
   )
